67f466da5bf07b33c5de55bfdeb0d1d35dd0ff1f
urw7rs <urw.7rs@icloud.com>
2021-08-31 21:39:08 +0900

    use histogram to find center point

--- src/line_follower.py
+++ src/line_follower.py
@@ -39,162 +39,99 @@ class Follower:
         img_color = cv2.resize(
             img_color, dsize=(320, 240), interpolation=cv2.INTER_AREA
         )
-        imgb, imgg, img_color = cv2.split(img_color)
-
-        # get size of image
-        height, width = img_color.shape[:2]
-
         # perspective transform
+        height, width, c = img_color.shape
         pts1 = np.float32([(123, 355), (491, 359), (30, 456), (624, 448)])
         pts2 = np.float32(
             [[10, 10], [width - 10, 10], [10, height - 10], [width - 10, height - 10]]
         )
 
-        cv2.circle(img_color, (int(pts1[0][0]), int(pts1[0][1])), 10, (255, 0, 0), -1)
-        cv2.circle(img_color, (int(pts1[1][0]), int(pts1[1][1])), 10, (255, 255, 0), -1)
-        cv2.circle(img_color, (int(pts1[2][0]), int(pts1[2][1])), 10, (255, 0, 255), -1)
-        cv2.circle(img_color, (int(pts1[3][0]), int(pts1[3][1])), 10, (0, 0, 255), -1)
-
         M = cv2.getPerspectiveTransform(pts1, pts2)
-        img_result = cv2.warpPerspective(img_original, M, (width, height))
 
-        # debug
-        # Perspective Transform result image
-        # cv2.imshow("perspective Transform result", img_result)
-
-        #####
-        # region_of_interest_vertices = [(0, height/2), (0, height), (width, height), (width, height / 2)]
-        # roi_img = region_of_interest(img_result, np.array([region_of_interest_vertices], np.int32))
-        # cv2.imshow('ffff',roi_img)
-
-        # Blur
+        img_result = cv2.warpPerspective(img_color, M, (width, height))
         img_result = cv2.bilateralFilter(img_result, 9, 75, 75)
 
         roi_img = cv2.cvtColor(img_result, cv2.COLOR_BGR2GRAY)
-
-        # Histogram equalizing
         roi_img = cv2.equalizeHist(roi_img)
-        # Opening
         roi_img = cv2.morphologyEx(
             roi_img, cv2.MORPH_OPEN, cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
         )
-        # Canny Edge
-        dst = cv2.Canny(roi_img, 40, 100, None, 3)
 
-        ##
-
-        # get hough line
-        linesP = cv2.HoughLinesP(dst, 1, np.pi / 180, 8, None, 30, 2)
-
-        slope_pos = np.empty((0, 0), dtype=float)
-        slope_neg = np.empty((0, 0), dtype=float)
-
-        right_count = 0
-        left_count = 0
-
-        # transfrom gray to BGR
+        dst = cv2.Canny(roi_img, 150, 300, None, 3)
         dst = cv2.cvtColor(dst, cv2.COLOR_GRAY2BGR)
 
-        if linesP is not None:
-            for i in range(0, len(linesP)):
-                l = linesP[i][0]
-                if l[2] - l[0] != 0:
-                    # get line's slope (degree)
-                    slope = math.atan(((l[1] - l[3]) / (l[2] - l[0]))) / math.pi * 180
+        # cv2.imshow("original", img_color)
+        # cv2.imshow('color',img_result)
+        # cv2.imshow('canny',dst)
 
-                    if slope >= 1:
-                        cv2.line(
-                            dst,
-                            (l[0], l[1]),
-                            (l[2], l[3]),
-                            (0, 255, 255),
-                            3,
-                            cv2.LINE_AA,
-                        )
-                        slope_pos = np.append(slope_pos, slope)
-                        right_count = right_count + 1
+        def getHistogram(img, minPer=0.1, display=False, region=1):
 
-                    elif slope <= -1:
-                        cv2.line(
-                            dst, (l[0], l[1]), (l[2], l[3]), (0, 255, 0), 3, cv2.LINE_AA
-                        )
-                        slope_neg = np.append(slope_neg, slope)
-                        left_count = left_count + 1
+            if region == 1:
+                histValues = np.sum(img, axis=0)  # axis=0 standard total
+            else:
+                histValues = np.sum(img[img_original.shape[0] // region :, :], axis=0)
 
-        if slope_pos.size == 0:
-            slope_pos = 90.0
-        if slope_neg.size == 0:
-            slope_neg = -90.0
+            # print("histValues:", histValues)
+            maxValue = np.max(histValues)
+            minValue = minPer * maxValue  # minimum percentatage
+            # print('minValue:',minValue)
 
-        right_steer = 90 - np.mean(slope_pos)
-        left_steer = -90 - np.mean(slope_neg)
+            indexArray = np.where(histValues >= minValue)
+            basePoint = int(np.average(indexArray))
+            # print('basepoint :',basePoint)  # middile point
 
-        if right_steer > 35:
-            right_steer = 35.01
-        if left_steer > 35:
-            left_steer = -35
+            if display:
+                imgHist = np.zeros(
+                    (img_color.shape[0], img_color.shape[1], 3), np.uint8
+                )
+                for x, intensity in enumerate(histValues):
+                    divided = np.linalg.norm(
+                        img_color.shape[0] - intensity // 255
+                    )  # // region
+                    cv2.line(
+                        imgHist,
+                        (x, img_color.shape[0]),
+                        (x, int(divided)),
+                        (255, 0, 255),
+                        1,
+                    )
+                    cv2.circle(
+                        imgHist,
+                        (basePoint, img_color.shape[0]),
+                        20,
+                        (0, 255, 255),
+                        cv2.FILLED,
+                    )
+                    # print('intensity:',intensity[0])
+                    # print('divided:',divided[0])
+                return basePoint, imgHist
+            return basePoint
 
-        if right_count > left_count:
-            steer_angle = right_steer
-        else:
-            steer_angle = left_steer
-
-        print(steer_angle)
-        rad_steer_angle = steer_angle / 180 * math.pi
-
-        height, width = dst.shape[:2]
-
-        # draw steering angle
-        cv2.line(
-            dst,
-            (int(width / 2), int(height / 2)),
-            ((int(width / 2), int(height / 2) - 150)),
-            (244, 5, 52),
-            3,
+        basePoint, imgHist = getHistogram(
+            img_result, display=True, minPer=0.5, region=4
         )
-        cv2.line(
-            dst,
-            (int(width / 2), int(height / 2)),
-            (
-                int(width / 2 + 100 * (math.sin((rad_steer_angle)))),
-                int(height / 2 - 100 * ((math.cos(rad_steer_angle)))),
-            ),
-            (0, 125, 255),
-            5,
-        )
-        cv2.circle(
-            dst,
-            (
-                int(width / 2 + 100 * (math.sin((rad_steer_angle)))),
-                int(height / 2 - 100 * ((math.cos(rad_steer_angle)))),
-            ),
-            10,
-            (0, 125, 255),
-            -1,
-        )
-        cv2.putText(
-            dst,
-            str(steer_angle),
-            (int(width / 2 + 10), int(height / 2 + 10)),
-            cv2.FONT_HERSHEY_SIMPLEX,
-            0.5,
-            (0, 0, 255),
-        )
-        #####
+        curveAveragePoint, imgHist = getHistogram(img_result, display=True, minPer=0.9)
+        ang_z = basePoint  # - curveAveragePoint
 
-        # result image(dst)
-        # cv2.imshow("dst", dst)
+        self.twist.linear.x = 0.1
+        self.twist.angular.z = ang_z
+        # curveRaw = curveAveragePoint-basePoint
 
-        cv2.waitKey(1)
+        # curveList = []
+        # avgValue = 10
+        # curveList.append(curveRaw)
+        # if len(curveList)> avgValue:    # make less noise
+        #     curveList.pop(0)
+        # curve = int(sum(curveList/len(curveList)))
 
-        rad_steer_angle = -rad_steer_angle
+        # # inverse step is skipped
 
-        self.twist.linear.x = 0.2
-        self.twist.angular.z = rad_steer_angle
+        # cv2.imshow("Histo", imgHist)
+
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
 
         self.cmd_vel_pub.publish(self.twist)
-        output_img = self.bridge.cv2_to_imgmsg(dst)
-        self.image_pub.publish(output_img)
 
         # END CONTROL
 
